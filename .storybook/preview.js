5d3059942a435de3d6c85a673614fbcaa10bbd9e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-23 15:19:21 -0300

    add storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,35 @@
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/styles/theme';
+import { BaseStyles } from '../src/styles/base';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.second,
+      },
+    ],
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <BaseStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
