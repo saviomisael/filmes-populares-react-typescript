ccf04e62d29362235eec8a34bdbc755029136e8c
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-23 16:48:40 -0300

    add Heading styles

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+type TitleProps = {
+  as: 'h1' | 'h2' | 'h3' | 'h4';
+  dark: boolean;
+  textCenter: boolean;
+};
+
+export const Title = styled.h1<TitleProps>`
+  color: ${({ theme, dark }) => (dark ? theme.colors.second : theme.colors.white)};
+  text-align: ${({ textCenter }) => (textCenter ? 'center' : 'left')};
+`;
