dad873e112bec72cfcb167c5e287128451357380
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-25 15:32:42 -0300

    pass test 'should renders image with alt text correct'

---
+++ src/components/TMDBLogo/TMDBLogo.spec.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from 'utils/test-utils';
+import { TMDBLogo } from '.';
+
+describe('<TMDBLogo />', () => {
+  it('should renders image with alt text correct', () => {
+    renderTheme(<TMDBLogo />);
+
+    const image = screen.getByAltText(/the movie db logo/i);
+
+    expect(image).toBeInTheDocument();
+  });
+});
