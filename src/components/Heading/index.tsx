2961a9656402be009c0cb170b72c6401cca97afb
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-25 14:30:29 -0300

    add uppercase styles to Heading component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -6,11 +6,18 @@ export type HeadingProps = {
   children: ReactNode;
   as?: 'h1' | 'h2' | 'h3' | 'h4';
   hasTextCenter?: boolean;
+  hasTextUppercase?: boolean;
 };
 
-export const Heading = ({ children, hasColorDark = false, as = 'h1', hasTextCenter = false }: HeadingProps) => {
+export const Heading = ({
+  children,
+  hasColorDark = false,
+  as = 'h1',
+  hasTextCenter = false,
+  hasTextUppercase = false,
+}: HeadingProps) => {
   return (
-    <Styled.Title dark={hasColorDark} as={as} textCenter={hasTextCenter}>
+    <Styled.Title dark={hasColorDark} as={as} textCenter={hasTextCenter} uppercase={hasTextUppercase}>
       {children}
     </Styled.Title>
   );
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -4,9 +4,11 @@ type TitleProps = {
   as: 'h1' | 'h2' | 'h3' | 'h4';
   dark: boolean;
   textCenter: boolean;
+  uppercase: boolean;
 };
 
 export const Title = styled.h1<TitleProps>`
   color: ${({ theme, dark }) => (dark ? theme.colors.second : theme.colors.white)};
   text-align: ${({ textCenter }) => (textCenter ? 'center' : 'left')};
+  text-transform: ${({ uppercase }) => (uppercase ? 'uppercase' : 'none')};
 `;
