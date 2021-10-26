ebb47af27f0905cc29028add39c9aae0d13415f6
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-26 12:28:52 -0300

    add tvSerieListMock

--- src/mocks/tv-serie-mock.ts
+++ src/mocks/tv-serie-mock.ts
@@ -1,1 +1,9 @@
 export const tvSerieMock = { id: 1, name: 'Round 6', rating: 7.5, poster: '/6gcHdboppvplmBWxvROc96NJnmm.jpg' };
+
+export const tvSerieListMock = [
+  { id: 1, name: 'Round 6', rating: 7.5, poster: '/6gcHdboppvplmBWxvROc96NJnmm.jpg' },
+  { id: 2, name: 'Round 6', rating: 7.5, poster: '/6gcHdboppvplmBWxvROc96NJnmm.jpg' },
+  { id: 3, name: 'Round 6', rating: 7.5, poster: '/6gcHdboppvplmBWxvROc96NJnmm.jpg' },
+  { id: 4, name: 'Round 6', rating: 7.5, poster: '/6gcHdboppvplmBWxvROc96NJnmm.jpg' },
+  { id: 5, name: 'Round 6', rating: 7.5, poster: '/6gcHdboppvplmBWxvROc96NJnmm.jpg' },
+];
