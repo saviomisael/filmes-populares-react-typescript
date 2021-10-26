65de77b442d0c4429a894e110f646e8e61ccbbb3
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-26 11:16:32 -0300

    add tv serie mock

---
+++ src/mocks/tv-serie-mock.ts
@@ -0,0 +1,1 @@
+export const tvSerieMock = { id: 1, name: 'Round 6', rating: 7.5, poster: '/6gcHdboppvplmBWxvROc96NJnmm.jpg' };
--- src/models/TvSerie.spec.ts
+++ src/models/TvSerie.spec.ts
@@ -1,9 +1,10 @@
 import { TvSerie } from './TvSerie';
+import { tvSerieMock } from 'mocks/tv-serie-mock';
 
 describe('TvSerie model', () => {
   it('should has poster that contain a url', () => {
-    const tvSerie = new TvSerie(1, 'breaking bad', 9.5, 'image.png');
+    const tvSerie = new TvSerie(tvSerieMock.id, tvSerieMock.name, tvSerieMock.rating, tvSerieMock.poster);
 
-    expect(tvSerie.poster).toMatch(/^https(.)+image\.png$/);
+    expect(tvSerie.poster).toMatch(/^https(.)+6gcHdboppvplmBWxvROc96NJnmm\.jpg$/);
   });
 });
