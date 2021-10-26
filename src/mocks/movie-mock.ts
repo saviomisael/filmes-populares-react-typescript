e05b3a52974389eab5f3158ae5208a9f49484b9f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-26 17:09:46 -0300

    add movie mock

---
+++ src/mocks/movie-mock.ts
@@ -0,0 +1,9 @@
+export const movieMock = {
+  id: 1,
+  name: 'Duna',
+  rating: 9,
+  releaseDate: new Date(),
+  poster: '/s9E9W77HS8zEQvsrpz5aEUTKnvD.jpg',
+  overview:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+};
