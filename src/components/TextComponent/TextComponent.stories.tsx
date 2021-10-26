66f13e66baba0ed6cef032b23e4a114de77e70ab
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-26 10:48:35 -0300

    refactor TextComponent stories

--- src/components/TextComponent/TextComponent.stories.tsx
+++ src/components/TextComponent/TextComponent.stories.tsx
@@ -23,19 +23,19 @@ export default {
 
 export const Default: Story<TextComponentProps> = (args) => <TextComponent {...args} />;
 
-export const WithTextSmall: Story<TextComponentProps> = Default.bind({});
+export const WithTextSmall = Default.bind({});
 
 WithTextSmall.args = {
   hasTextSmall: true,
 };
 
-export const WithTooMuchText: Story<TextComponentProps> = Default.bind({});
+export const WithTooMuchText = Default.bind({});
 
 WithTooMuchText.args = {
   children: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
 };
 
-export const WithTextCentered: Story<TextComponentProps> = Default.bind({});
+export const WithTextCentered = Default.bind({});
 
 WithTextCentered.args = {
   hasTextCenter: true,
