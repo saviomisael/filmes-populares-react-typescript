9183415d9ce2186aa0d4f8f61d8e9b1ddef16ead
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-26 15:32:06 -0300

    pass test 'should renders component with background black'

--- src/components/Section/Section.spec.tsx
+++ src/components/Section/Section.spec.tsx
@@ -7,4 +7,10 @@ describe('<Section />', () => {
 
     expect(container.firstChild).toHaveStyleRule('background', '#FA9F42');
   });
+
+  it('should renders component with background black', () => {
+    const { container } = renderTheme(<Section bgColor="black">Some text</Section>);
+
+    expect(container.firstChild).toHaveStyleRule('background', '#413B3B');
+  });
 });
