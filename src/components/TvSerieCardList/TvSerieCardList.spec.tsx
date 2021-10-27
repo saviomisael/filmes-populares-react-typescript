2b019723bf1a6c91702eaeac7cbb0f0ca3c406a5
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-27 14:19:59 -0300

    pass test 'should render many list item elements'

--- src/components/TvSerieCardList/TvSerieCardList.spec.tsx
+++ src/components/TvSerieCardList/TvSerieCardList.spec.tsx
@@ -5,9 +5,17 @@ import { tvSerieListMock } from 'mocks/tv-serie-mock';
 import { TvSerie } from 'models/TvSerie';
 
 describe('<TvSerieCardList />', () => {
-  it('should matches with snapshot', () => {
-    const series = tvSerieListMock.map((x) => new TvSerie(x.id, x.name, x.rating, x.poster));
+  const series = tvSerieListMock.map((x) => new TvSerie(x.id, x.name, x.rating, x.poster));
 
+  it('should render many list item elements', () => {
+    renderTheme(<TvSerieCardList series={series} />);
+
+    const liElements = screen.getAllByRole('listitem');
+
+    expect(liElements).toHaveLength(5);
+  });
+
+  it('should matches with snapshot', () => {
     renderTheme(<TvSerieCardList series={series} />);
 
     const container = screen.getByTestId('tvserielistcontainer');
