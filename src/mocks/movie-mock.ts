2df49af454beffefe99bd131575767fb5afc3fcb
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-27 13:53:37 -0300

    add movieListMock

--- src/mocks/movie-mock.ts
+++ src/mocks/movie-mock.ts
@@ -7,3 +7,42 @@ export const movieMock = {
   overview:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 };
+
+export const movieListMock = [
+  {
+    id: 1,
+    name: 'Duna',
+    rating: 9,
+    releaseDate: new Date(),
+    poster: '/s9E9W77HS8zEQvsrpz5aEUTKnvD.jpg',
+    overview:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    id: 2,
+    name: 'Duna',
+    rating: 9,
+    releaseDate: new Date(),
+    poster: '/s9E9W77HS8zEQvsrpz5aEUTKnvD.jpg',
+    overview:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    id: 3,
+    name: 'Duna',
+    rating: 9,
+    releaseDate: new Date(),
+    poster: '/s9E9W77HS8zEQvsrpz5aEUTKnvD.jpg',
+    overview:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    rating: 9,
+    releaseDate: new Date(),
+    poster: '/s9E9W77HS8zEQvsrpz5aEUTKnvD.jpg',
+    overview:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+];
