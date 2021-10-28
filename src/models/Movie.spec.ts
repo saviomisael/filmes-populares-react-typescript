149f63ffba73d6aa96b0d35e5bb55cf3ca05fab3
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-28 11:11:11 -0300

    pass test 'should has a empty array when genres are nullish'

--- src/models/Movie.spec.ts
+++ src/models/Movie.spec.ts
@@ -14,4 +14,17 @@ describe('Movie model', () => {
 
     expect(movie.poster).toMatch(/^https(.)+s9E9W77HS8zEQvsrpz5aEUTKnvD\.jpg$/);
   });
+
+  it('should has a empty array when genres are nullish', () => {
+    const movie = new Movie(
+      movieMock.id,
+      movieMock.name,
+      movieMock.rating,
+      movieMock.releaseDate,
+      movieMock.poster,
+      movieMock.overview,
+    );
+
+    expect(movie.genres).toHaveLength(0);
+  });
 });
