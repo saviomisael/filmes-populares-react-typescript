5ad069cd719b74100d22441afcd0d3b03bc0b12e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-28 10:08:32 -0300

    add LANGUAGE REQUEST constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const IMAGE_URL = process.env.REACT_APP_IMAGE_URL;
 export const API_URL = process.env.REACT_APP_API_URL;
 export const API_KEY = process.env.REACT_APP_API_KEY;
+export const LANGUAGE_REQUEST = 'pt-BR';
