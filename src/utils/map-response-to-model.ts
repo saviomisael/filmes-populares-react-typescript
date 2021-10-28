55c4e2b4f1e9f68e547fb377b9330d7ed8f56398
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-28 11:22:38 -0300

    add mapResponseToMovieModel function

--- src/utils/map-response-to-model.ts
+++ src/utils/map-response-to-model.ts
@@ -1,4 +1,6 @@
+import { Movie } from 'models/Movie';
 import { TvSerie } from 'models/TvSerie';
+import { MovieResponse } from 'services/responses/MovieResponse';
 import { TvSerieResponse } from 'services/responses/TvSerieResponse';
 
 const mapSingleResponseToTvSerieModel = (response: TvSerieResponse) =>
@@ -6,3 +8,17 @@ const mapSingleResponseToTvSerieModel = (response: TvSerieResponse) =>
 
 export const mapResponseToTvSerieModel = (response: TvSerieResponse[]) =>
   response.map((x) => mapSingleResponseToTvSerieModel(x));
+
+const mapSingleResponseToMovieModel = (response: MovieResponse) =>
+  new Movie(
+    response.id,
+    response.title,
+    response.vote_average,
+    new Date(response.release_date),
+    response.poster_path,
+    response.overview,
+    response.genres,
+  );
+
+export const mapResponseToMovieModel = (response: MovieResponse[]) =>
+  response.map((x) => mapSingleResponseToMovieModel(x));
