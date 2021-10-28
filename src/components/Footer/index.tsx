659c0e37f2826c0d905180e34dbc0cb93682a521
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-28 14:08:45 -0300

    refactor Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import { ExternalLink } from 'components/ExternalLink';
 import { TextComponent } from 'components/TextComponent';
 import { TMDBLogo } from 'components/TMDBLogo';
 import * as Styled from './styles';
@@ -6,7 +7,9 @@ export const Footer = () => {
   return (
     <Styled.FooterContainer data-testid="footer">
       <TextComponent>Dados providos por:</TextComponent>
-      <TMDBLogo />
+      <ExternalLink to="https://developers.themoviedb.org/3/getting-started/introduction">
+        <TMDBLogo />
+      </ExternalLink>
     </Styled.FooterContainer>
   );
 };
