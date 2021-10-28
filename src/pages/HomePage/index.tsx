b0f96c187176888dbec442a418f032b5dd1135c2
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-28 12:29:35 -0300

    refactor HomePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,8 +1,24 @@
 import { SectionMovies } from 'components/SectionMovies';
 import { SectionTvSeries } from 'components/SectionTvSeries';
+import { useAppDispatch } from 'hooks/use-app-dispatch';
 import { PageTemplate } from 'pages/PageTemplate';
+import { useEffect } from 'react';
+import { fetchMoviesThunk } from 'store/ducks/movie-duck';
+import { fetchTvSeriesThunk } from 'store/ducks/tv-serie-duck';
 
 export const HomePage = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const tvSeriesActionPromise = dispatch(fetchTvSeriesThunk());
+    const moviesActionPromise = dispatch(fetchMoviesThunk());
+
+    return () => {
+      tvSeriesActionPromise.abort();
+      moviesActionPromise.abort();
+    };
+  }, [dispatch]);
+
   return (
     <PageTemplate>
       <SectionTvSeries />
