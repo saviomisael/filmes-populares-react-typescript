5abcc3a2337d0e252b7f2deff61bf01f7462c249
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-28 14:56:38 -0300

    add GenreBadgeList styles

---
+++ src/components/GenreBadgeList/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
