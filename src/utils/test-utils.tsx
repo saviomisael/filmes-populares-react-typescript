5f1cd41e220fcf176ddeab9b7a533bb1d51ec41f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-28 13:55:31 -0300

    add renderWithRouter

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,10 +1,16 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
 import { BaseStyles } from 'styles/base';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { storeMock } from 'mocks/configure-store-mock';
+
+type optionsWithRouter = {
+  initialRoutes?: string[];
+  pathname: string;
+};
 
 export const renderTheme = (children: ReactNode) =>
   render(
@@ -23,3 +29,19 @@ export const renderWithStoreProvider = (children: ReactNode) =>
       </ThemeProvider>
     </Provider>,
   );
+
+export const renderWithRouter = (children: ReactNode, options: optionsWithRouter) => {
+  const initialRoutes = options.initialRoutes && options.initialRoutes.length > 0 ? options.initialRoutes : ['/'];
+
+  return render(
+    <Provider store={storeMock}>
+      <ThemeProvider theme={theme}>
+        <MemoryRouter initialEntries={initialRoutes}>
+          <Switch>
+            <Route path={options.pathname} render={() => children} />
+          </Switch>
+        </MemoryRouter>
+      </ThemeProvider>
+    </Provider>,
+  );
+};
