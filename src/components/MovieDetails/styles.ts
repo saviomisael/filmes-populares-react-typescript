bfa14cafcfc11f32e3b0df6647f27e0ece6f7a3f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-29 11:29:19 -0300

    add MovieDetails styles

---
+++ src/components/MovieDetails/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 70vw;
+  display: flex;
+`;
+
+export const LeftContent = styled.aside`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 5rem;
+  row-gap: 1.3rem;
+`;
+
+export const Poster = styled.img`
+  width: 50%;
+`;
