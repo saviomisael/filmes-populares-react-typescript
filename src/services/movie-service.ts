0fd87d6b1538e853670553f0685a206e03eac3de
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-30 10:45:55 -0300

    add getMovieById

--- src/services/movie-service.ts
+++ src/services/movie-service.ts
@@ -1,9 +1,19 @@
 import { API_KEY, LANGUAGE_REQUEST } from 'utils/constants';
-import { mapResponseToMovieModel } from 'utils/map-response-to-model';
+import { mapResponseToMovieModel, mapSingleResponseToMovieModel } from 'utils/map-response-to-model';
 import { api } from './api';
+
+interface MovieByIdBody {
+  id: number;
+}
 
 export const getMoviesMostPopular = async () => {
   const response = await api.get(`/movie/popular?api_key=${API_KEY}&language=${LANGUAGE_REQUEST}`);
 
   return mapResponseToMovieModel(response.data.results);
+};
+
+export const getMovieById = async ({ id }: MovieByIdBody) => {
+  const response = await api.get(`/movie/${id}?api_key=${API_KEY}&language=${LANGUAGE_REQUEST}`);
+
+  return mapSingleResponseToMovieModel(response.data);
 };
