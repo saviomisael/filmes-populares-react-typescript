7737313dcfe9d2dca00cb5ff2eae7b515342949f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-30 11:15:09 -0300

    pass test 'should update single movie'

--- src/store/ducks/movie-duck.spec.ts
+++ src/store/ducks/movie-duck.spec.ts
@@ -1,4 +1,4 @@
-import { movieListMock } from 'mocks/movie-mock';
+import { movieListMock, movieMock } from 'mocks/movie-mock';
 import { Movie } from 'models/Movie';
 import { movieReducer, movieActions, MovieState } from './movie-duck';
 
@@ -6,6 +6,7 @@ describe('movie duck', () => {
   it('should add movies', () => {
     const initialState: MovieState = {
       movies: [],
+      singleMovie: null,
     };
 
     const payload = movieListMock.map((x) => new Movie(x.id, x.name, x.rating, x.releaseDate, x.poster, x.overview));
@@ -13,5 +14,31 @@ describe('movie duck', () => {
     const stateModified = movieReducer(initialState, movieActions.addMovies(payload));
 
     expect(stateModified.movies).toHaveLength(4);
+  });
+
+  it('should update single movie', () => {
+    const initialState: MovieState = {
+      movies: [],
+      singleMovie: null,
+    };
+
+    const payload = new Movie(
+      movieMock.id,
+      movieMock.name,
+      movieMock.rating,
+      movieMock.releaseDate,
+      movieMock.poster,
+      movieMock.overview,
+    );
+
+    const { singleMovie } = movieReducer(initialState, movieActions.updateSingleMovie(payload));
+
+    expect(singleMovie?.id).toBe(1);
+    expect(singleMovie?.name).toBe('Duna');
+    expect(singleMovie?.rating).toBe(9);
+    expect(singleMovie?.releaseDate).toEqual(new Date(2021, 1, 15));
+    expect(singleMovie?.poster).toMatch(/\.jpg$/);
+    expect(singleMovie?.overview).toMatch(/^Lorem ipsum(.)+consequat\.$/);
+    expect(singleMovie?.genres).toHaveLength(0);
   });
 });
