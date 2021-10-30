cf9ee305e8d41bd1420262d65553270c5450a3a0
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-30 11:20:07 -0300

    update movie duck mock

--- src/mocks/movie-duck-mock.ts
+++ src/mocks/movie-duck-mock.ts
@@ -1,13 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Movie } from 'models/Movie';
-import { movieListMock } from './movie-mock';
-
-export interface MovieState {
-  movies: Movie[];
-}
+import { MovieState } from 'store/ducks/movie-duck';
+import { genresMock } from './genre-mock';
+import { movieListMock, movieMock } from './movie-mock';
 
 const initialState: MovieState = {
   movies: movieListMock.map((x) => new Movie(x.id, x.name, x.rating, x.releaseDate, x.poster, x.overview)),
+  singleMovie: new Movie(
+    movieMock.id,
+    movieMock.name,
+    movieMock.rating,
+    movieMock.releaseDate,
+    movieMock.poster,
+    movieMock.overview,
+    [...genresMock],
+  ),
 };
 
 const movieSlice = createSlice({
@@ -16,6 +23,9 @@ const movieSlice = createSlice({
   reducers: {
     addMovies: (state, action: PayloadAction<Movie[]>) => {
       state.movies = action.payload;
+    },
+    updateSingleMovie: (state, action: PayloadAction<Movie>) => {
+      state.singleMovie = action.payload;
     },
   },
 });
